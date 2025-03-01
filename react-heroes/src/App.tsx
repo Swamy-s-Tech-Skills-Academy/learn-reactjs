23280535165f565578832fd13a77382c7ca1e05d
Viswanatha Swamy <vishipayyallore@yahoo.com>
2025-03-01 11:42:46 +0530

    Swamy/28feb work (#2)
    * Update README.md
    
    * Initial Version
    
    * Installation
    
    * Update README.md
    
    * Changes
    
    * Changes
    
    * Package
    
    * Update App.css
    
    * Update App.tsx

--- react-heroes/src/App.tsx
+++ react-heroes/src/App.tsx
@@ -2,12 +2,29 @@ import './App.css'
 
 function App() {
 
+  const hero = {
+    id: 1,
+    name: 'Batman',
+  };
+
   return (
     <>
-      <div>
-        <h1 className="text-3xl font-bold underline">
-          Hello world!
-        </h1>
+      <div className='container mt-5 mx-auto'>
+        <h2 className='text-2xl'>Details</h2>
+
+        <div>
+          <span className='font-bold'>ID:</span> {hero.id}
+        </div>
+        <div className='space-x-2'>
+          <span className='font-bold'>Name:</span>
+          <span className='uppercase'>{hero.name}</span>
+        </div>
+
+        <div className='flex flex-col gap-2 mt-3 border-t'>
+          <label>Hero name:</label>
+          <input type='text' placeholder='Hero Name please' className='border border-gray-300 rounded-lg p-2 w-1/4' />
+        </div>
+
       </div>
     </>
   )
