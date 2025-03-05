d99a4c96a5b693b617b91970f8283c34797fbf46
Viswanatha Swamy <vishipayyallore@yahoo.com>
2025-03-05 21:32:25 +0530

    Update App.tsx

--- react-heroes/src/App.tsx
+++ react-heroes/src/App.tsx
@@ -1,10 +1,20 @@
+import { ChangeEvent, useState } from 'react';
 import './App.css'
+
+type Hero = {
+  id: number;
+  name: string;
+}
 
 function App() {
 
-  const hero = {
+  const [hero, setHero] = useState<Hero>({
     id: 1,
     name: 'Batman',
+  });
+
+  const handleNameChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setHero({ ...hero, name: e.target.value });
   };
 
   return (
@@ -17,6 +27,15 @@ function App() {
       <div className="space-x-2">
         <span className="font-bold">Name:</span>
         <span className='uppercase'>{hero.name}</span>
+      </div>
+
+      <div className="flex flex-col gap-2 mt-3 border-t">
+        <label>Hero Name:</label>
+        <input type="text" placeholder='Hero name'
+          className="border border-gray-300 rounded-lg p-2 w-1/4"
+          value={hero.name}
+          onChange={handleNameChange}
+        />
       </div>
     </div>
   )
