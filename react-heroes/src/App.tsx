ba6abb6d36f784fead5abe9eda3ce1097488a590
Viswanatha Swamy <vishipayyallore@yahoo.com>
2025-03-05 20:46:06 +0530

    Changes

--- react-heroes/src/App.tsx
+++ react-heroes/src/App.tsx
@@ -2,11 +2,17 @@ import './App.css'
 
 function App() {
 
+  const hero = {
+    id: 1,
+    name: 'Batman',
+  };
+
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="container mt-5 mx-auto">
+      <h2 className='text-2xl'>Details</h2>
+      <h1>Hero: {hero.name}</h1>
+    </div>
   )
 }
 
-export default App
+export default App;
--- react-heroes/tailwind.config.js
+++ react-heroes/tailwind.config.js
@@ -5,6 +5,17 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    container: {
+      center: true,
+      padding: '1rem', // Optional: adds horizontal padding
+      screens: {
+        sm: '100%',
+        md: '100%',
+        lg: '1200px',  // Adjust as needed
+        xl: '1400px',  // Adjust as needed
+        '2xl': '1600px', // Instead of a fixed 1536px, you can set a custom value
+      },
+    },
     extend: {},
   },
   plugins: [],
