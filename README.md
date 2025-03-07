b0cd4ee077efb0ad8a9f72fb80b19c220cc8ac8c
Viswanatha Swamy <vishipayyallore@yahoo.com>
2025-03-08 00:37:16 +0530

    Swamy/05mar work (#3)
    * Update App.tsx
    
    * Changes
    
    * Changes
    
    * Initial Version
    
    * Package Updates
    
    * Tailwind
    
    * Update README.md
    
    * INitial Setup
    
    * Update App.tsx
    
    * Changes
    
    * Displaying Hero
    
    * Update App.tsx

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ I am learning React.js from different Video Courses, Books, and Websites.
 
 ## Reference(s)
 
+> 1. <https://v3.tailwindcss.com/docs/guides/vite>
 > 1. <https://react.dev/learn>
 > 1. <https://tailwindcss.com/>
 > 1. <https://vite.dev/>
@@ -13,9 +14,19 @@ I am learning React.js from different Video Courses, Books, and Websites.
 
 ```powershell
 npm create vite@latest
+npm create vite@latest react-heroes
 
-npm install tailwindcss @tailwindcss/vite
-npm install tailwindcss @tailwindcss/postcss postcss
-
+npm install -D tailwindcss@3 postcss autoprefixer
 npx tailwindcss init -p
+```
+
+## Trouble shooting
+
+```powershell
+Remove-Item -Recurse -Force node_modules
+Remove-Item package-lock.json
+
+npm install
+
+Get-ChildItem .\node_modules\.bin\
 ```
--- react-heroes/src/App.tsx
+++ react-heroes/src/App.tsx
@@ -1,33 +1,44 @@
+import { ChangeEvent, useState } from 'react';
 import './App.css'
+
+type Hero = {
+  id: number;
+  name: string;
+}
 
 function App() {
 
-  const hero = {
+  const [hero, setHero] = useState<Hero>({
     id: 1,
     name: 'Batman',
+  });
+
+  const handleNameChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setHero({ ...hero, name: e.target.value });
   };
 
   return (
-    <>
-      <div className='container mt-5 mx-auto'>
-        <h2 className='text-2xl'>Details</h2>
-
-        <div>
-          <span className='font-bold'>ID:</span> {hero.id}
-        </div>
-        <div className='space-x-2'>
-          <span className='font-bold'>Name:</span>
-          <span className='uppercase'>{hero.name}</span>
-        </div>
-
-        <div className='flex flex-col gap-2 mt-3 border-t'>
-          <label>Hero name:</label>
-          <input type='text' placeholder='Hero Name please' className='border border-gray-300 rounded-lg p-2 w-1/4' />
-        </div>
-
+    <div className="container mt-5 mx-auto">
+      <h2 className='text-2xl'>Details</h2>
+      <div>
+        <span className="font-bold">ID:</span> {hero.id}
       </div>
-    </>
+
+      <div className="space-x-2">
+        <span className="font-bold">Name:</span>
+        <span className='uppercase'>{hero.name}</span>
+      </div>
+
+      <div className="flex flex-col gap-2 mt-3 border-t">
+        <label>Hero Name:</label>
+        <input type="text" placeholder='Hero name'
+          className="border border-gray-300 rounded-lg p-2 w-1/4"
+          value={hero.name}
+          onChange={handleNameChange}
+        />
+      </div>
+    </div>
   )
 }
 
-export default App
+export default App;
