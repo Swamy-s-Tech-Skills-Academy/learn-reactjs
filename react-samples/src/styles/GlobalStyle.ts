4a678f58636a62d96c321f77e8fc35d3b80f1721
Viswanatha Swamy <vishipayyallore@yahoo.com>
2025-04-28 11:11:11 +0530

    Changes

---
+++ react-samples/src/styles/GlobalStyle.ts
@@ -0,0 +1,23 @@
+/**
+ * Global styles for the application.
+ * 
+ * Note: This file is primarily for documentation purposes.
+ * The actual global styles are applied through Tailwind CSS
+ * in src/index.css using Tailwind directives.
+ * 
+ * If you need additional global styles beyond Tailwind's defaults,
+ * consider adding them to index.css using @layer base.
+ */
+
+// Export the empty object to allow importing this file without side effects
+export default {};
+
+/**
+ * Example of how to add global styles in index.css:
+ * 
+ * @layer base {
+ *   body {
+ *     @apply m-0 p-0 font-sans;
+ *   }
+ * }
+ */
