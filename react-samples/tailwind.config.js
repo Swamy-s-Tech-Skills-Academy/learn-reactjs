c88a3f1a57c131a731fab074fea608dbf4757f83
Viswanatha Swamy <vishipayyallore@yahoo.com>
2025-04-28 09:40:58 +0530

    Changes

--- react-samples/tailwind.config.js
+++ react-samples/tailwind.config.js
@@ -1,7 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
+    container: {
+      center: true,
+      padding: '1rem', // Optional: adds horizontal padding
+      screens: {
+        sm: '100%',
+        md: '100%',
+        lg: '1200px',  // Adjust as needed
+        xl: '1500px',  // Adjust as needed
+        '2xl': '1600px', // Instead of a fixed 1536px, you can set a custom value
+      },
+    },
     extend: {},
   },
   plugins: [],
